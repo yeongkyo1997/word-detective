e314f41686d2379839713a95ca878f1f76b2f934
dayounglee <53906423+dayoung100@users.noreply.github.com>
2023-09-13 14:58:49 +0900

    Feat: 프론트엔드 폴더 추가
    - 리액트 네비게이션, 리덕스 툴킷, 스타일드 컴포넌트 라이브러리 추가
    - 네비게이션 기본 뼈대 추가

---
+++ frontend/App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./pages/etc/Login";
+import Main from "./pages/home/Main";
+import PictureLobby from "./pages/pictureGame/PictureLobby";
+import WordLobby from "./pages/wordGame/WordLobby";
+import LetterLobby from "./pages/letterGame/LetterLobby";
+import WordNoteMain from "./pages/wordNote/WordNoteMain";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar hidden={true} />
+      <Stack.Navigator
+        initialRouteName="Main"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="PictureLobby" component={PictureLobby} />
+        <Stack.Screen name="WordLobby" component={WordLobby} />
+        <Stack.Screen name="LetterLobby" component={LetterLobby} />
+        <Stack.Screen name="WordNoteMain" component={WordNoteMain} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ frontend/pages/wordNote/WordNoteMain.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const WordNoteMain = () => {
+  return (
+    <View>
+      <Text>단어장 메인화면</Text>
+    </View>
+  );
+};
+export default WordNoteMain;
