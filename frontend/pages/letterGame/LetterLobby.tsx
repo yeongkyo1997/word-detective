8d59ad8d9d85b1b73bffe3f4fb640fb4cce08e2b
nevermindperil <123xxx@kakao.com>
2023-09-14 14:47:46 +0900

    Modify : header변경

--- frontend/pages/letterGame/LetterLobby.tsx
+++ frontend/pages/letterGame/LetterLobby.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from "react-native";
-
-const LetterLobby = () => {
+import Header from "../etc/Header";
+const LetterLobby = ({ navigation }: any) => {
   return (
     <View>
+      <Header navigation={navigation} />
       <Text>단어 쪼개기의 로비화면</Text>
     </View>
   );
