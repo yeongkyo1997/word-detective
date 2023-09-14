489a1ca69822317d162479ac7683dddc69c3d72c
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-09-14 11:12:22 +0900

    Feat: 이미지 업로드 - 이미지 중복 업로드 문제 해결 - 이미지 url 불러오는 기능 추가 - 중복 업로드 시 기존의 이미지 삭제하고 새로운 이미지로 덮어쓰기

--- backend/src/main/java/com/ssafy/backend/repository/PhotoRepository.java
+++ backend/src/main/java/com/ssafy/backend/repository/PhotoRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.backend.repository;
 
 import com.ssafy.backend.entity.Photo;
+import com.ssafy.backend.entity.User;
+import com.ssafy.backend.entity.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    Optional<Photo> findByUserAndWord(User user, Word word);
+    long deleteByUserAndWord(User user, Word word);
 }
--- backend/src/main/java/com/ssafy/backend/service/S3Service.java
+++ backend/src/main/java/com/ssafy/backend/service/S3Service.java
@@ -1,6 +1,9 @@
 package com.ssafy.backend.service;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.ssafy.backend.entity.Photo;
 import com.ssafy.backend.entity.User;
@@ -11,6 +14,7 @@ import com.ssafy.backend.repository.WordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -22,10 +26,13 @@ public class S3Service {
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+    @Value("${cloud.aws.region.static}")
+    private String region;
     private final PhotoRepository photoRepository;
     private final UserRepository userRepository;
     private final WordRepository wordRepository;
 
+    @Transactional
     public String uploadFile(Long userId, Long wordId, MultipartFile file) throws IOException {
         User user = userRepository.findById(userId).orElseThrow(
                 () -> new IllegalArgumentException("해당 유저가 없습니다.")
@@ -34,9 +41,18 @@ public class S3Service {
         Word word = wordRepository.findById(wordId).orElseThrow(
                 () -> new IllegalArgumentException("해당 단어가 없습니다.")
         );
+        Photo prePhoto = photoRepository.findByUserAndWord(user, word).orElse(null);
+
+        if (prePhoto != null) {
+            photoRepository.deleteByUserAndWord(user, word);
+            fileDelete(prePhoto.getUrl());
+        }
 
         String fileName = file.getOriginalFilename();
-        String fileUrl = "https://" + bucket + "/test" + fileName.replaceAll(" ", "");
+        String fileUrl = null;
+        if (fileName != null) {
+            fileUrl = "https://" + bucket + ".s3." + region + ".amazonaws.com/" + fileName.replaceAll(" ", "");
+        }
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(file.getContentType());
         metadata.setContentLength(file.getSize());
@@ -50,5 +66,20 @@ public class S3Service {
                 .build());
 
         return fileUrl;
+    }
+
+    private void fileDelete(String fileUrl) {
+        try {
+            String key = fileUrl.substring(58);
+            AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
+
+            try {
+                s3.deleteObject(bucket, key);
+            } catch (AmazonServiceException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception exception) {
+            throw new IllegalArgumentException("해당 파일이 없습니다.");
+        }
     }
 }
