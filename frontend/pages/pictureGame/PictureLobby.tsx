2f21ad21a4543805f84104e61512462fb23893e0
wnstj7788 <wnstj7788@gmail.com>
2023-09-14 15:07:01 +0900

    test

--- frontend/pages/pictureGame/PictureLobby.tsx
+++ frontend/pages/pictureGame/PictureLobby.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const PictureLobby = () => {
   return (
     <View>
-      <Text>그림 맞추기의 로비화면</Text>
+      <Text>그림 맞추기의 로비화면 입니다</Text>
     </View>
   );
 };
