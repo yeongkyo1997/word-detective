d34546788c42bf1ff114e7cc2542c206fcf34dea
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-09-21 10:39:24 +0900

    Feat : IOException 전역 에러처리 - hadleIOException을 구현하여 IOException을 처리
    Former-commit-id: d574385b4dccaa7f76efa6b9e173fcf4c43cefb0

--- backend/src/main/java/com/ssafy/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/backend/exception/GlobalExceptionHandler.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.io.IOException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e, WebRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getCode(), e.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ErrorResponse> handleIOException(IOException e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.FILE_NOT_FOUND.getCode(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
