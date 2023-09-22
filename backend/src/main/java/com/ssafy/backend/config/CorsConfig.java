5612da5f32ab79bb265c2a9ecfb67909297f859a
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-09-22 16:44:22 +0900

    Feat: cors 에러 설정
    Former-commit-id: fabf8786a25db94fa949da02423244f421a1d624

---
+++ backend/src/main/java/com/ssafy/backend/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.ssafy.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // cors를 적용할 URL 패턴 정의
+                .allowedOriginPatterns("*") // 모든 ip에 대해서 허용
+                .allowedMethods("*"); // 허용할 http method 지정
+
+    }
+}
