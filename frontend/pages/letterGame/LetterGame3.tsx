ed59d549438d069eebabd02ed6961861fd30b294
human646581@gmail.com <human646581@gmail.com>
2023-09-22 16:35:14 +0900

    단어 나누기 3단계 수정
    Former-commit-id: ba7e5f8920086e9350db3d2401931ef8ab325069

--- frontend/pages/letterGame/LetterGame3.tsx
+++ frontend/pages/letterGame/LetterGame3.tsx
@@ -4,26 +4,17 @@ import styled from "styled-components/native";
 import { ContainerBg } from "../../styles/globalStyles";
 import { RouteProp, useRoute } from "@react-navigation/native";
 import { RootStackParamList } from "../../App";
-// import { Sketch } from 'expo-pixi';
+import LetterCanvas from "./LetterCanvas";
 type StagePageRouteProp = RouteProp<RootStackParamList, "LetterGame2">;
-
 // @ts-ignore
-// const { width, height } = Dimensions.get('window');
+import * as hangul from 'hangul-js';
+
 const LetterGame3 = () => {
 
   const route = useRoute<StagePageRouteProp>();
   const {word} = route.params;
-  // const onReady = async ({gl, sketch }) => {
-  //   // You can do something when the sketch is ready
-  //   console.log('Sketch is ready!');
-  // };
-  //
-  // const onChangeAsync = async ({gl, sketch }) => {
-  //   // This gets triggered whenever the user draws
-  //   // You can save the drawing if needed
-  //   const { uri } = await sketch.takeSnapshotAsync();
-  //   console.log(uri);
-  // };
+  const list : string[]=hangul.disassemble(word.name);
+  console.log(list);
   return (
     <ContainerBg source={require("../../assets/background/game/fruit.png")}>
       <Container>
@@ -46,15 +37,8 @@ const LetterGame3 = () => {
           </Progress>
         </ContainerA>
         <ContainerB>
-          {/*<Sketch*/}
-          {/*  style={{ flex: 1 }}*/}
-          {/*  strokeWidth={10}  // The width of the brush stroke*/}
-          {/*  strokeColor="blue"  // The color of the brush stroke*/}
-          {/*  onChangeAsync={onChangeAsync}*/}
-          {/*  onReady={onReady}*/}
-          {/*/>*/}
+          <LetterCanvas list={list}></LetterCanvas>
         </ContainerB>
-
       </Container>
     </ContainerBg>
   );
@@ -125,13 +109,9 @@ const ContainerA = styled.View`
 const ContainerB = styled.View`
   flex:1;
   margin:3%;
-  background-color: white;
-  border: black;
+  background-image: image("../../assets/card/fruit/apple.png");
+  border: 1px black;
   border-radius: 20px;
-  ${Platform.OS === 'android' && `
-    elevation: 10;
-  `}
-  overflow: hidden;
 `
 
 export default LetterGame3;
