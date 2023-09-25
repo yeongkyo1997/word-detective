717ce40daec5e586131b11a62f804904b7d74142
wnstj7788 <wnstj7788@gmail.com>
2023-09-25 15:36:18 +0900

    audio
    Former-commit-id: a6efbfa2dc0cdea18fd15a7e276a3aa963782659

--- frontend/pages/tutorial/TutorialOne.tsx
+++ frontend/pages/tutorial/TutorialOne.tsx
@@ -7,12 +7,31 @@ import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import Header from "../etc/Header";
 import {Container, ContainerBg, MenuBtn} from "../../styles/globalStyles";
 import MiddleSet from "../components/MiddleSet";
+import {Audio} from "expo-av";
+import {useEffect} from "react"; // Expo Audio 라이브러리 추가
 
 type RootStackNavigationProp = NativeStackNavigationProp<RootStackParamList>;
 
 const Main = () => {
     const isLoaded = useCachedResources();
     const navigation = useNavigation<RootStackNavigationProp>();
+    useEffect(() => {
+        // 페이지가 렌더링될 때 소리를 자동으로 재생
+        const playSound = async () => {
+            const soundObject = new Audio.Sound();
+            try {
+                await soundObject.loadAsync(require("../../assets/mav/01_안녕_나는_단어탐정이야_내가_모아놓은_단어카드를_잃어버렸는데_도와줘.wav"));
+                await soundObject.playAsync();
+            } catch (error) {
+                console.error("소리 재생 중 오류 발생:", error);
+            }
+        };
+
+        // 컴포넌트가 마운트될 때 소리를 재생
+        playSound();
+    }, []); // 빈 배열을 두어 한 번만 실행되도록 설정
+
+
 
     if (isLoaded) {
         return (
