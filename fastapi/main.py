a68694594ce56b49685c05898dc641a1fbaca705
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-09-26 17:22:32 +0900

    Feat : 이미지 세그멘테이션 엔드포인트 지정
    Former-commit-id: 260b9eb8939e78d9c613dd51d9a0416a11f9f728

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,46 @@
-from fastapi import FastAPI
+import os
 
+import boto3
+from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile, HTTPException
+
+from config.cors_config import setup_cors
+
+load_dotenv()
 app = FastAPI()
+setup_cors(app)
 
 
 @app.get("/fast")
 async def root():
     return {"message": "Hello World"}
+
+
+access_key = os.getenv("access_key")
+secret_key = os.getenv("secret_key")
+region_name = os.getenv("region_name")
+bucket_name = os.getenv("bucket_name")
+
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    region_name=region_name,
+)
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+    file_location = f"uploads/{file.filename}"
+    try:
+        s3_client.upload_fileobj(
+            file.file,
+            bucket_name,
+            file_location,
+        )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=e)
+
+    file_url = f"https://{bucket_name}.s3.{region_name}.amazonaws.com/{file_location}"
+    return {"file_url": file_url}
