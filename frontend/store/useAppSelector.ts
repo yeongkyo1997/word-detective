99c8a3153f7c375a569091d350675ac47a0d8286
dayounglee <dayoung015@gmail.com>
2023-09-28 14:41:24 +0900

    Fix:불필요 코드 삭제, 주석 형식 변경
    Former-commit-id: 7a706578e4b8f0ade14988fbc5da9eb1a9ef2cee

--- frontend/store/useAppSelector.ts
+++ frontend/store/useAppSelector.ts
@@ -4,8 +4,8 @@ import { RootState } from "./store";
 
 // export const useAppDispatch: () => AppDispatch = useDispatch;
 /**
- * useSelector 대신 사용하는 훅
- * 매번 타입 지정해줘야하는 번거로움 해결
+ * useSelector 대신 사용하는 훅.
+ * 매번 타입 지정해줘야하는 번거로움 해결.
  * const user = useSelector((state: RootState) => state.user); 대신에
  * const user = useAppSelector((state) => state.user); 와 같이 사용하면 됨
  */
