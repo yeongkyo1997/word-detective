9e8121bb25d419a8ef867589e9d687311f8c5c9f
dayounglee <dayoung015@gmail.com>
2023-09-29 18:18:41 +0900

    Fix:stageCard에서 url로 이미지 보여주도록 변경
    Former-commit-id: 6a65bbfb059fd8cad32a7b3eeeebdefa10e6869e

--- frontend/pages/components/StageCard.tsx
+++ frontend/pages/components/StageCard.tsx
@@ -12,30 +12,6 @@ type RootStackNavigationProp = NativeStackNavigationProp<RootStackParamList>;
 const StageCard = (props: { stage: IStage; gameType: string }) => {
   const isLoaded = useCachedResources();
   const navigation = useNavigation<RootStackNavigationProp>();
-  function getImage(name: string): any {
-    switch (name) {
-      case "사과":
-        return require("../../assets/card/fruit/apple.png");
-      case "오렌지":
-        return require("../../assets/card/fruit/orange.png");
-      case "수박":
-        return require("../../assets/card/fruit/watermelon.png");
-      case "토마토":
-        return require("../../assets/card/fruit/tomato.png");
-      case "체리":
-        return require("../../assets/card/fruit/cherry.png");
-      case "바나나":
-        return require("../../assets/card/fruit/banana.png");
-      case "딸기":
-        return require("../../assets/card/fruit/strawberry.png");
-      case "멜론":
-        return require("../../assets/card/fruit/melon.png");
-      case "복숭아":
-        return require("../../assets/card/fruit/peach.png");
-      case "포도":
-        return require("../../assets/card/fruit/grapes.png");
-    }
-  }
 
   const cardStyle = {
     borderWidth: 7,
@@ -65,7 +41,7 @@ const StageCard = (props: { stage: IStage; gameType: string }) => {
           <EmptyStar source={require("../../assets/etc/star2.png")} />
         )}
         <StageImgWrap>
-          <StageImg source={getImage(props.stage.word.name)} resizeMode={"contain"}></StageImg>
+          <StageImg source={{ uri: props.stage.word.url }} resizeMode={"contain"}></StageImg>
         </StageImgWrap>
         <StageText key={props.stage.word.name}>{props.stage.word.name}</StageText>
       </CardContainer>
