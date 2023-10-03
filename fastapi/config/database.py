35e1ec49349a7e4acffa2148781a75c49e6fb511
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-10-03 16:49:18 +0900

    Feat : requirements.txt 최신화
    Former-commit-id: 5baa980a37d12917104fb33bc7ad37733f2ad929

--- fastapi/config/database.py
+++ fastapi/config/database.py
@@ -1,11 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# MYSQL_HOST = "j9b105.p.ssafy.io"
-# MYSQL_USER = "root"
-# MYSQL_PASSWORD = "detection-password"
-# MYSQL_DATABASE = "detection"
-
 MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
 MYSQL_PASSWORD = "root"
