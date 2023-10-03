aef4fb2929549ddfbcca480f2be55afa745d27d1
dayounglee <53906423+dayoung100@users.noreply.github.com>
2023-10-03 14:31:26 +0900

    Fix: word2에 api 뺀 테스트 로직 추가-로딩 시간 확인용
    Former-commit-id: c0921a074859984706a7c894bd5d4697fffb7ae4

--- frontend/pages/wordGame/WordGame2.tsx
+++ frontend/pages/wordGame/WordGame2.tsx
@@ -45,6 +45,57 @@ const WordGame2 = () => {
   const [shuffledDropList, setShuffledDropList] = useState<IWordWithDone[]>([]); //리스트를 섞기
   const [shuffledDragList, setShuffledDragList] = useState<IWord[]>([initialWord]); //리스트를 섞기
 
+  //--- api 연결 안했을 때 ---
+  // useEffect(() => {
+  //   const choiceList: IWord[] = [
+  //     {
+  //       id: 1,
+  //       name: "사과",
+  //       url: "",
+  //     },
+  //     {
+  //       id: 1,
+  //       name: "사과",
+  //       url: "",
+  //     },
+  //     {
+  //       id: 1,
+  //       name: "사과",
+  //       url: "",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "바나나",
+  //       url: "",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "딸기",
+  //       url: "",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "토마토",
+  //       url: "",
+  //     },
+  //   ];
+  //   const dropRandList = shuffleArray(choiceList);
+  //   const dragRandList = shuffleArray(choiceList);
+  //   let dropList: IWordWithDone[] = []; //드롭될 위치들
+  //   dropRandList.map((word, index) => {
+  //     word.index = index;
+  //     dropList.push({
+  //       word: word,
+  //       done: false,
+  //     });
+  //   });
+  //   dragRandList.map((word, index) => {
+  //     word.index = index;
+  //   });
+  //   setShuffledDropList(dropList);
+  //   setShuffledDragList(dragRandList);
+  // }, []);
+
   useEffect(() => {
     let choiceList: IWord[] = [];
     //선지 8개 배열 choiceList에
