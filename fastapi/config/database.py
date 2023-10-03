d63b79072417c296bb50a00a64e551bdbb865cc3
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-10-03 16:42:11 +0900

    Feat : requirements.txt 최신화
    Former-commit-id: fa5c1ac2849a1e66e12250ca96de7ec0a4be17cb

--- fastapi/config/database.py
+++ fastapi/config/database.py
@@ -11,7 +11,7 @@ MYSQL_USER = "root"
 MYSQL_PASSWORD = "root"
 MYSQL_DATABASE = "detection"
 DATABASE_URL = (
-    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
+    f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
 )
 
 
