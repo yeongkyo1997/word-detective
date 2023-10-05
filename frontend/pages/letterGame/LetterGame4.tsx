3682f39b7c738b11386f01579e5965a47228dc7c
dayounglee <53906423+dayoung100@users.noreply.github.com>
2023-10-05 15:16:40 +0900

    Feat:letter clear api 적용
    Former-commit-id: 68a0a8e6fd9e3ebfc523a7f085d8dba1429fc18c

--- frontend/pages/letterGame/LetterGame4.tsx
+++ frontend/pages/letterGame/LetterGame4.tsx
@@ -1,5 +1,13 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { View, ImageBackground, PanResponder, TouchableOpacity, Platform, Text, Image} from "react-native";
+import React, { useRef, useEffect, useState } from "react";
+import {
+  View,
+  ImageBackground,
+  PanResponder,
+  TouchableOpacity,
+  Platform,
+  Text,
+  Image,
+} from "react-native";
 import styled from "styled-components/native";
 import { ContainerBg } from "../../styles/globalStyles";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
@@ -7,29 +15,45 @@ import { RootStackParamList } from "../../App";
 import LetterCanvas from "./LetterCanvas";
 type StagePageRouteProp = RouteProp<RootStackParamList, "LetterGame2">;
 // @ts-ignore
-import * as hangul from 'hangul-js';
+import * as hangul from "hangul-js";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 type RootStackNavigationProp = NativeStackNavigationProp<RootStackParamList>;
-import axios from 'axios';
+import axios from "axios";
 import Modal from "react-native-modal";
 import GetCardModal from "../components/GetCardModal";
+import { UserAPI } from "../../utils/api";
+import useAppSelector from "../../store/useAppSelector";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/user";
+
 const LetterGame4 = () => {
   const [write, setWrite] = useState("");
   const navigation = useNavigation<RootStackNavigationProp>();
   const route = useRoute<StagePageRouteProp>();
-  const {word} = route.params;
-  const list : string[]=hangul.disassemble(word.name);
-  const click=()=>{
-
-  }
+  const { word } = route.params;
+  const list: string[] = hangul.disassemble(word.name);
+  const click = () => {};
   const [isModalVisible, setModalVisible] = useState(false);
-  // @ts-ignrore
-  useEffect(()=>{
-    if(word.name===write){
-      setModalVisible(true);
+  const user = useAppSelector(state => state.user.value);
+  const dispatch = useDispatch();
 
+  // @ts-ignrore
+  useEffect(() => {
+    if (word.name === write) {
+      //api연결
+      UserAPI.stageClear({ ...user, letter: word.id })
+        .then(res => {
+          dispatch(login(res.data));
+        })
+        .then(() => {
+          //모달 오픈
+          setModalVisible(true);
+        })
+        .catch(e => {
+          console.log("스테이지 클리어 api 관련 에러 발생: ", e);
+        });
     }
-  }, [write])
+  }, [write]);
 
   return (
     <ContainerBg source={require("../../assets/background/game/fruit.png")}>
@@ -47,27 +71,28 @@ const LetterGame4 = () => {
         <GetCardModal nextScreen="Main" word={word}></GetCardModal>
       </Modal>
       <ContainerA>
-        <Text style={{fontSize:20, fontFamily:"BMJUA", textAlign:"center"}}>{word.name}</Text>
-        <Text style={{textAlign:"center", fontFamily:"BMJUA"}}>적은 글자 : {write}</Text>
-
+        <Text style={{ fontSize: 20, fontFamily: "BMJUA", textAlign: "center" }}>{word.name}</Text>
+        <Text style={{ textAlign: "center", fontFamily: "BMJUA" }}>적은 글자 : {write}</Text>
       </ContainerA>
-      <LetterCanvas list={list}
-                        pointer={0}
-                        alpha={false}
-                        // @ts-ignore
-                        setWrite={setWrite}></LetterCanvas>
+      <LetterCanvas
+        list={list}
+        pointer={0}
+        alpha={false}
+        // @ts-ignore
+        setWrite={setWrite}
+      ></LetterCanvas>
     </ContainerBg>
   );
-}
+};
 
 const ContainerA = styled.View`
- position: absolute;
+  position: absolute;
   top: 0;
   left: 0;
   width: 20%;
   height: 20%;
   z-index: 4;
   justify-content: center;
-`
+`;
 
 export default LetterGame4;
